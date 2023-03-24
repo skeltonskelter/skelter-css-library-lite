1b5041c5d8efc6ab5264631e22e1997563352513
Elton Chim <skeltonskelter@yahoo.com>
2023-03-25 06:48:22 +0800

    Added all the files

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+// npm init
+// -to create a package.json file.
+
+// npm install gulp gulp-sass sass --save-dev
+// -to install gulp, gulp-sass for compiling and sass package to package.json as dev dependencies.
+
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
+
+function buildStyles() {
+  return src("sass/**/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
+}
+
+function watchTask() {
+  watch(["sass/**/*.scss", "*.html"], buildStyles);
+}
+
+exports.default = series(buildStyles, watchTask);
